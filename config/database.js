547bc13606ee0f521226dd488380fe7c39bcb630
nikunjdev22 <113764661+nikunjdev22@users.noreply.github.com>
2022-09-17 19:05:56 +0530

    host name chnages

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'projectestimation'),
       user: env('DATABASE_USERNAME', 'admin'),
