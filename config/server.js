ecfa8a45162f0d25ad4380d722231a3f69701ffa
nikunjdev22 <113764661+nikunjdev22@users.noreply.github.com>
2022-09-17 18:19:16 +0530

    add heroku url

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '10.0.0.1'),
-  port: env.int('PORT', 1338),
+  host: env('https://dashboard.heroku.com/apps/estimationxcode', '10.0.0.1'),
+  port: env.int('PORT', 3306),
   app: {
     keys: env.array('APP_KEYS'),
   },
