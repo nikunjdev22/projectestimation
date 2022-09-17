7518190fbffb881ac160ea67a4bfee1c185330e4
nikunjdev22 <113764661+nikunjdev22@users.noreply.github.com>
2022-09-17 18:03:58 +0530

    mysql database

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'projectestimation'),
-      user: env('DATABASE_USERNAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'admin'),
       password: env('DATABASE_PASSWORD', 'admin'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      // ssl: {
+      //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      // },
     },
+     debug: false,
   },
 });
