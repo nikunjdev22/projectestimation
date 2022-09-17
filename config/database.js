c921566e587176648f2d155963e0c02d0e0209fe
nikunjdev22 <113764661+nikunjdev22@users.noreply.github.com>
2022-09-17 17:54:40 +0530

    add host name

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'projectestimation'),
       user: env('DATABASE_USERNAME', 'admin'),
