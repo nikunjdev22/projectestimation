94a0682978ed6c6887368c6467f9a79cdb061636
nikunjdev22 <113764661+nikunjdev22@users.noreply.github.com>
2022-09-17 18:26:38 +0530

    chnage server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
+
 module.exports = ({ env }) => ({
-  host: env('https://dashboard.heroku.com/apps/estimationxcode', '10.0.0.1'),
-  port: env.int('PORT', 3306),
+  host: env('HOST', '10.0.0.1'),
+  port: env.int('PORT', 1338),
   app: {
     keys: env.array('APP_KEYS'),
   },
